59843429afbd61710c342b116bc231a948923267
agustinmar <agustin_mar@hotmail.com>
2017-08-01 15:43:32 -0500

    equinox_31JUL2017

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "rho.interaxes.org", user: "equinox@hipicolasilla.com", roles: %w{app db web}, my_property: :my_value
+server "64.150.189.20", user: "equinox@hipicolasilla.com", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
