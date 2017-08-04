ad55e0f989ce6ba8c345e6decb389a9530049fce
agustinmar <agustin_mar@hotmail.com>
2017-08-04 12:59:06 -0500

    equinox_31JUL2017

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "rho.interaxes.org:2022", user: "deploy", roles: %w{app db web}, my_property: :my_value
-server "rho.interaxes.org:2022", user: "deploy", :app, :web, :db, :primary => true
+server "rho.interaxes.org:2022", user: "deploy", roles: %w{app db web}
 set :deploy_to, "/current"
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
