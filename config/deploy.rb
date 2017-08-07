e3fc3653dd08ee545a2aacca795f1fa9307fc7ea
agustinmar <agustin_mar@hotmail.com>
2017-08-07 17:36:33 -0500

    equinox_31JUL2017

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
  require 'mina/bundler'
  require 'mina/rails'
  require 'mina/git'
- require 'mina/deploy'
  require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
@@ -54,7 +53,7 @@ end
 desc "Deploys the current version to the server."
 task :deploy do
   # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
+  invoke :'git:ensure_pushed'
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
